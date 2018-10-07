453a0014a4a8245a78f23f16989b7011100388bc
hadley31 <nick.hadley31@gmail.com>
2018-10-07 00:52:31 -0400

    pickup changes

---
+++ Assets/_Nocturnal Forest/Scripts/Utility/EventTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventTimer : MonoBehaviour
+{
+	public float duration;
+	public UnityEvent onFire;
+
+	private float m_Timer;
+
+	private void Update()
+	{
+		if (m_Timer <= 0)
+		{
+			onFire.Invoke();
+			m_Timer = duration;
+		}
+		else
+		{
+			m_Timer -= Time.deltaTime;
+		}
+	}
+}
