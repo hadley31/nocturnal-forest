de7c86469913f99fca2fd083481716c30b47ebc4
hadley31 <nick.hadley31@gmail.com>
2018-10-11 01:12:14 -0400

    changes

---
+++ Assets/_Nocturnal Forest/Scripts/Enemy/Wizard/WizardBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardBehaviour : EnemyBase
+{
+	[Header("Warp")]
+	public float maxWarpDistance = 15.0f;
+	public float warpCooldownTime = 5.0f;
+
+	private float m_NextAllowedWarpTime;
+
+	private void Update()
+	{
+
+	}
+
+	public void Warp()
+	{
+		if (Time.time < m_NextAllowedWarpTime)
+		{
+			return;
+		}
+
+		
+	}
+}
