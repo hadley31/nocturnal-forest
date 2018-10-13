8989dc624576ecc7bbfdcee93efdc1b8714684ac
= <nick.hadley31@gmail.com>
2018-10-13 14:26:12 -0400

    manabar

--- Assets/_Nocturnal Forest/Scripts/Character/Character.cs
+++ Assets/_Nocturnal Forest/Scripts/Character/Character.cs
@@ -12,8 +12,26 @@ public class Character : CharacterBase
         private set;
     }
 
+    private int m_XP;
+
+    public IntUnityEvent onXpChanged;
+
     public UnityEvent onSpawn;
     public UnityEvent onDie;
+
+
+    public int XP
+    {
+        get { return m_XP; }
+        set
+        {
+            if (m_XP != value)
+            {
+                m_XP = value;
+
+            }
+        }
+    }
 
     private void OnEnable()
     {
@@ -40,11 +58,13 @@ public class Character : CharacterBase
         }
     }
 
-    public void Spawn(){
+    public void Spawn()
+    {
         onSpawn.Invoke();
     }
 
-    public void Die(){
+    public void Die()
+    {
         onDie.Invoke();
     }
 }
---
+++ Assets/_Nocturnal Forest/Scripts/UI/CharacterManabar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterManabar : MonoBehaviour
+{
+    private Slider m_Slider;
+
+    private void Awake()
+    {
+        m_Slider = GetComponent<Slider>();
+    }
+
+
+    private void OnEnable()
+    {
+        Character.Current?.onXpChanged?.AddListener(OnChanged);
+    }
+
+    private void OnDisable()
+    {
+        Character.Current?.onXpChanged?.RemoveListener(OnChanged);
+    }
+
+    private void OnChanged(int value)
+    {
+        m_Slider.value = value;
+    }
+}
